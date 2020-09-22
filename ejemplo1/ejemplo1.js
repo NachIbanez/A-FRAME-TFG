64e38085c6f8d5991e536669cc7c13ea29217c19
NachIbanez <43443842+NachIbanez@users.noreply.github.com>
2020-09-23 00:27:49 +0200

    ejemplo1-dia1

---
+++ ejemplo1/ejemplo1.js
@@ -0,0 +1,51 @@
+//Componente para cambiar el color de una entidad al interactuar con el ratón sobre éste
+
+AFRAME.registerComponent('change-color-on-hover', {
+    schema: {
+      color: {default: 'green'}
+    },
+
+    init: function () {
+      var data = this.data;
+      var el = this.el;
+      var defaultColor = el.getAttribute('material').color;
+
+      el.addEventListener('mouseenter', function () {
+        el.setAttribute('color', data.color);
+      });
+
+      el.addEventListener('mouseleave', function () {
+        el.setAttribute('color', defaultColor);
+      });
+
+      el.addEventListener('click', function () {
+        el.setAttribute('color', "red");
+      });
+    }
+  });
+
+//Componente para modificar el DOM con queryselector y set/get getAttribute
+
+AFRAME.registerComponent('foo', {
+        init: function () {
+          // Jugar con las entidades de la escena
+          var sceneEl = document.querySelector('a-scene');
+          var boxEl = document.querySelector('#caja');
+          console.log(sceneEl);
+          console.log(sceneEl.querySelectorAll('a-entity'));
+          console.log(sceneEl.querySelector('a-box'));
+          console.log(sceneEl.querySelectorAll('a-sphere, a-cylinder'));
+          console.log(sceneEl.querySelectorAll('#polygon'));
+
+          sceneEl.addEventListener('click', function () {
+            var condition1 = boxEl.getAttribute('animation');
+            console.log(condition1 == null)
+            if (condition1 == null){
+                boxEl.setAttribute('animation', "property: position; from: -1 0.5 -3; to: -1 1.5 -3; dir: alternate; loop: true");
+            }else{
+                boxEl.removeAttribute('animation');
+                posicion_actual = boxEl.getAttribute('position');
+            };
+          });
+        }
+      });
